97db74a5fa87ebc39dd96e79ca1472130e6165ac
LuisFernando93 <luispasquinelli@hotmail.com>
2026-07-18 02:45:20 -0300

    MyActor added and implemented to perform a simple math equation on C++
    MyActor is a C++ script created to perform a simple math equation and study the basics of how to create C++ scripts

---
+++ Source/EbacProject/EbacProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EbacProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EbacProject, "EbacProject" );
---
+++ Source/EbacProject/Private/MyActor.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	constante = 5;
+}
+
+float AMyActor::EquacaoNumerica(float x)
+{
+	float y = 2 * x + constante;
+	return y;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
